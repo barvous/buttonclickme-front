10636b30d8ad2e269bb62ea8334571e279cfb614
Marcos Daniel <marcostkd20147@gmail.com>
2024-12-09 21:01:39 -0300

    Principal component created

---
+++ buttonclickme-front/src/app/tela-principal/tela-principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-principal',
+  imports: [],
+  templateUrl: './tela-principal.component.html',
+  styleUrl: './tela-principal.component.css'
+})
+export class TelaPrincipalComponent {
+
+}
