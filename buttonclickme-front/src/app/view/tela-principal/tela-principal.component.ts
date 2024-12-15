27a43186c5e9d80582a99f1f3cc7d60fc0a92a66
Marcos Daniel <marcostkd20147@gmail.com>
2024-12-15 11:59:57 -0300

    Adding delay in the saveCounter function to eventually save in DB

--- buttonclickme-front/src/app/view/tela-principal/tela-principal.component.ts
+++ buttonclickme-front/src/app/view/tela-principal/tela-principal.component.ts
@@ -10,11 +10,15 @@ import { LocalStorageCounterService } from '../../service/LocalStorageService';
   templateUrl: './tela-principal.component.html',
   styleUrl: './tela-principal.component.css',
   providers: [LocalStorageCounterService],
-
 })
 export class TelaPrincipalComponent implements OnInit {
   userCounter: number = 0;
+  mainUserCounter: number = 0;
   globalCounter: number = 0;
+  numberToAdd: number = 0;
+
+  private saveTimeout: any;
+  private SAVE_DELAY = 1500; // 1.5 segundos
 
   constructor(private localStorageService: LocalStorageCounterService) {}
 
@@ -23,14 +27,45 @@ export class TelaPrincipalComponent implements OnInit {
   }
 
   private initCounters() {
-    this.userCounter = this.localStorageService.getUserCounter();
+    let currentUserCounter: number = this.localStorageService.getUserCounter();
+    this.userCounter = currentUserCounter;
+    this.mainUserCounter = currentUserCounter;
 
     this.globalCounter = this.localStorageService.getGlobalCounter();
   }
 
-  public incrementCounter(): void {
-    this.userCounter = this.localStorageService.incrementUserCounter(1);
+  public saveCounters() {
+    this.numberToAdd++;
+    this.mainUserCounter++;
 
-    this.globalCounter = this.localStorageService.incrementGlobalCounter(1);
+    this.localStorageService.incrementUserCounter(1);
+    this.localStorageService.incrementGlobalCounter(1);
+
+    this.initTimeToSaveInDB(this.numberToAdd);
+  }
+
+  private initTimeToSaveInDB(numberToAdd: number): void {
+    // Limpa o temporizador anterior
+    if (this.saveTimeout) {
+      clearTimeout(this.saveTimeout);
+    }
+
+    // Inicia um novo temporizador
+    this.saveTimeout = setTimeout(() => {
+      this.saveCountersInDB(numberToAdd);
+    }, this.SAVE_DELAY);
+  }
+
+  private saveCountersInDB(numberToAdd: number): void {
+    console.log('Salvando contadores no banco de dados...');
+
+    this.userCounter = this.localStorageService.getUserCounter();
+    this.globalCounter = this.localStorageService.getGlobalCounter();
+
+    console.log(
+      `User Counter: ${this.userCounter}, Global Counter: ${this.globalCounter}`
+    );
+
+    this.numberToAdd = 0;
   }
 }
