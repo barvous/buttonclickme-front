f998e4f3300e8d3b77b4cc268b2002029aae56d1
Marcos Daniel <marcostkd20147@gmail.com>
2024-12-15 10:38:15 -0300

    Aligning vertically the viewboard

--- buttonclickme-front/src/app/app.component.ts
+++ buttonclickme-front/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { TelaPrincipalComponent } from "./tela-principal/tela-principal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TelaPrincipalComponent],
+  imports: [TelaPrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
