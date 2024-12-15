e3377715d3921990eee5bbf2ed65fdd247f135a9
Marcos Daniel <marcostkd20147@gmail.com>
2024-12-15 11:32:57 -0300

    Creating LocalStorageService file

---
+++ buttonclickme-front/src/app/service/LocalStorageService.ts
@@ -0,0 +1,39 @@
+import { ConstantLocalStorageKeys } from '../constant/LocalStorageKeys';
+
+export class LocalStorageCounterService {
+  public getUserCounter(): number {
+    let userCounterCacheValue: string | null = localStorage.getItem(
+      ConstantLocalStorageKeys.USER_COUNTER
+    );
+
+    return userCounterCacheValue ? parseInt(userCounterCacheValue) : 0;
+  }
+
+  public getGlobalCounter(): number {
+    let globalCounterCacheValue: string | null = localStorage.getItem(
+      ConstantLocalStorageKeys.GLOBAL_COUNTER
+    );
+
+    return globalCounterCacheValue ? parseInt(globalCounterCacheValue) : 0;
+  }
+
+  public incrementUserCounter(numberToAdd: number): number {
+    let newUserCounter = this.getUserCounter() + numberToAdd;
+    localStorage.setItem(
+      ConstantLocalStorageKeys.USER_COUNTER,
+      newUserCounter.toString()
+    );
+
+    return newUserCounter;
+  }
+
+  public incrementGlobalCounter(numberToAdd: number): number {
+    let newGlobalCounter = this.getGlobalCounter() + numberToAdd;
+    localStorage.setItem(
+      ConstantLocalStorageKeys.GLOBAL_COUNTER,
+      newGlobalCounter.toString()
+    );
+
+    return newGlobalCounter;
+  }
+}
